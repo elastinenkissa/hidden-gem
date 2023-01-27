aaa1c2d2ae45fb58d8640b9343d8d1f1b1bd22bf
Minel Drda <mineldrdait@gmail.com>
2023-01-27 20:34:30 +0100

    Removed reduntant theme props

--- src/theme.ts
+++ src/theme.ts
@@ -10,44 +10,44 @@ export const theme: Theme = {
   text: {
     size: {
       default: 15,
-      title: 20,
+      title: 20
     },
     weight: {
       default: 'normal',
-      title: 'bold',
-    },
+      title: 'bold'
+    }
   },
-  dimensions: {
-    navigation: {
-      height: 40,
-      borderWidth: 0.2,
-    },
-  },
+  // dimensions: {
+  //   navigation: {
+  //     height: 40,
+  //     borderWidth: 0.2,
+  //   },
+  // },
   colors: {
-    global: {
-      stats: {
-        border: 'cyan'
-      }
-    },
+    // global: {
+    //   stats: {
+    //     border: 'cyan'
+    //   }
+    // },
     default: {
       text: '#20232a',
       background: {
         primary: 'white',
-        secondary: '#e1e5e8',
+        secondary: '#e1e5e8'
       },
       border: {
-        color: '#9c9ea2',
-      },
+        color: '#9c9ea2'
+      }
     },
     dark: {
       text: 'white',
       background: {
         primary: '#20232a',
-        secondary: '#272a31',
+        secondary: '#272a31'
       },
       border: {
-        color: '#9c9ea2',
-      },
-    },
-  },
+        color: '#9c9ea2'
+      }
+    }
+  }
 };
